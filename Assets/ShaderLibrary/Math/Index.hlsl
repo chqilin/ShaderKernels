e64878ff7f8568441495d20807b5a8c529139393
Qilin <qilin.cheng@foxmail.com>
2022-01-25 23:55:50 +0800

    @1.0.4

---
+++ Assets/ShaderLibrary/Math/Index.hlsl
@@ -0,0 +1,11 @@
+
+#ifndef _SHADERFAB_MATH_INDEX_HLSL_
+#define _SHADERFAB_MATH_INDEX_HLSL_
+
+#include "./Basic.hlsl"
+#include "./Random.hlsl"
+#include "./Matrix.hlsl"
+#include "./Geometry.hlsl"
+#include "./Curve.hlsl"
+
+#endif//_SHADERFAB_MATH_INDEX_HLSL_
